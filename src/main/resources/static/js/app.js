53b71500189a1cbca868af3b8904ff850aa4a546
Jcro15 <juan.rojas-o@mail.escuelaing.edu.co>
2020-02-26 10:03:47 -0500

    entrega inicial

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,11 @@
+var Module=(function (){
+    var funcion =function (){
+        var author=$("#nombreUsuario").val();
+    	alert(author);
+    }
+
+      return {
+        funcion:funcion
+      };
+
+})();
